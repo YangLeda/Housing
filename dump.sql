bae10c380b09415c1fbaaaa2ddefb2c684c5dc69
杨乐达 <yangleda@outlook.com>
2017-10-04 23:05:52 +1100

    dump.sql   deploy

--- dump.sql
+++ dump.sql
@@ -142,7 +142,7 @@ CREATE TABLE `landlord` (
   `fullname` varchar(45) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id_UNIQUE` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -216,4 +216,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-10-03 15:14:08
+-- Dump completed on 2017-10-04 23:00:16
